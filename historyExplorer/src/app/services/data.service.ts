f5304d50d1f0a568cb933cf14d71a18625bec733
khalidchbab <kiko_loko@hotmail.fr>
2022-01-06 03:39:36 +0100

    Almost done with WebsiteRanking

--- historyExplorer/src/app/services/data.service.ts
+++ historyExplorer/src/app/services/data.service.ts
@@ -3,7 +3,6 @@ import {
 } from '@angular/core';
 import * as d3 from 'd3';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -12,13 +11,7 @@ export class DataService {
 
   constructor() {
   }
-
-  
-  get_covid(){
-    return d3.csv("https://www.data.gouv.fr/fr/datasets/r/63352e38-d353-4b54-bfd1-f1b3ee1cabd7")
-  }
-
-  get_map(){
-    return d3.json("https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements-version-simplifiee.geojson")
+  getHistory(){
+    return d3.csv("../../../assets/_files/idk.csv")
   }
 }
