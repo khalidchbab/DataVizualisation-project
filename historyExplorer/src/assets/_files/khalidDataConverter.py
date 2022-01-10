9c5d5b91e9aaea5d8debf4541c43edf205ba991c
khalidchbab <kiko_loko@hotmail.fr>
2022-01-10 23:14:38 +0100

    Changes

--- historyExplorer/src/assets/_files/khalidDataConverter.py
+++ historyExplorer/src/assets/_files/khalidDataConverter.py
@@ -5,8 +5,8 @@ def extract_website(row):
     return urlparse(row.url).netloc
 
 
-df = pd.read_csv("./OuhmaidDataRow.csv")
+df = pd.read_csv("./DrioucheDataRaw.csv")
 
 df["website"] = df.apply(lambda row: extract_website(row),axis=1)
 
-df.to_csv('OuhmaidData.csv', sep=",")
+df.to_csv('DrioucheData.csv', sep=",")
