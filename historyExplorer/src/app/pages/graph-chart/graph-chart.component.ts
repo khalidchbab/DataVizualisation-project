19d6b9a0a0e1f060ec43b043c41e11d92d4a1143
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2022-01-10 22:22:41 +0100

    Addin graphChart V2.2 !

--- historyExplorer/src/app/pages/graph-chart/graph-chart.component.ts
+++ historyExplorer/src/app/pages/graph-chart/graph-chart.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { PieChartService } from './../../services/pie-chart.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  PieChartService
+} from './../../services/pie-chart.service';
 import * as d3 from 'd3';
 
 @Component({
@@ -11,29 +16,29 @@ export class GraphChartComponent {
 
   graph: any;
   svg: any;
-  draggedNode : any ; 
-  simulation : any ; 
-  link : any ;
-  node : any ; 
+  draggedNode: any;
+  simulation: any;
+  link: any;
+  node: any;
   private color: any;
-  private width = 600 ; 
-  private height = 200 ; 
+  private width = 600;
+  private height = 200;
 
 
-  constructor(private graphChart: PieChartService) { 
+  constructor(private graphChart: PieChartService) {
 
     this.graphChart.get_graph().then((res: any) => {
 
-      this.graph = res ;
+      this.graph = res;
       this.svg = d3.select("figure#graph")
-                    .append("svg")
-                    .attr("width", this.width)
-                    .attr("height", this.height)
-                    .append("g")
-                    // .attr(
-                    //   "transform",
-                    //   "translate(" + this.width / 2 + "," + this.height / 2 + ")"
-                    // );
+        .append("svg")
+        .attr("width", this.width)
+        .attr("height", this.height)
+        .append("g")
+      // .attr(
+      //   "transform",
+      //   "translate(" + this.width / 2 + "," + this.height / 2 + ")"
+      // );
 
       this.color = d3.scaleOrdinal(d3.schemeCategory10);
       this.drawGraph(this.graph)
@@ -45,67 +50,73 @@ export class GraphChartComponent {
       // for (let i = 0; i < 3; i++) { 
       //   console.log(this.colors[i]);
       // }
-      
+
       // console.log(this.getUnique(this.graph.nodes));
-      var legendAux = ['Education', 'Coding', 'SocialMedia', 'Search', 'Other', 'Entertainment']  
+      var legendAux = ['Education', 'Coding', 'SocialMedia', 'Search', 'Other', 'Entertainment']
       // var legendAux = []  
-  
-      this.svg.append('g')
-              .selectAll('rect')
-              // .selectAll("circle")
-              .data(legendAux)
-              .enter()
-              // .append("circle")
-              .append("rect")
-              // .attr("cx", 5)
-              // .attr("cy",(d: any) => { return ((legendAux.indexOf(d) + 1)*20) +30; })
-              // .attr("r", 5)
-              .attr("x", 0)
-              .attr("y", (d: any) => { return ((legendAux.indexOf(d) + 1)*20) + 25; })
-              .attr("width", 10)
-              .attr("height", 10)
-              .style("fill", (d: any) => { return this.color(legendAux.indexOf(d) + 1); })
 
       this.svg.append('g')
-              // .selectAll("circle")
-              .selectAll('rect')
-              .data(legendAux)
-              .enter()
-              .append("text")
-              .attr("x", 15)
-              .attr("y", (d: any) => { return ((legendAux.indexOf(d) + 1)*20) +30; })
-              .text((d: any) => (d))
-              .style("font-size", "15px")
-              .attr("alignment-baseline","middle")
+        .selectAll('rect')
+        // .selectAll("circle")
+        .data(legendAux)
+        .enter()
+        // .append("circle")
+        .append("rect")
+        // .attr("cx", 5)
+        // .attr("cy",(d: any) => { return ((legendAux.indexOf(d) + 1)*20) +30; })
+        // .attr("r", 5)
+        .attr("x", 0)
+        .attr("y", (d: any) => {
+          return ((legendAux.indexOf(d) + 1) * 20) + 25;
+        })
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", (d: any) => {
+          return this.color(legendAux.indexOf(d) + 1);
+        })
 
-              var users = ['#user1', '#user2', '#user3']
+      this.svg.append('g')
+        // .selectAll("circle")
+        .selectAll('rect')
+        .data(legendAux)
+        .enter()
+        .append("text")
+        .attr("x", 15)
+        .attr("y", (d: any) => {
+          return ((legendAux.indexOf(d) + 1) * 20) + 30;
+        })
+        .text((d: any) => (d))
+        .style("font-size", "15px")
+        .attr("alignment-baseline", "middle")
 
-              users.forEach( el => {
-                // d3.select("figure#graph").exit().remove();
-                
-                d3.select(el)
-                .on("change", () => {
-                  this.cleanSVG() ; 
-                  this.drawGraph(this.randomData())  ; 
-                })
-              })
+      var users = ['#user1', '#user2', '#user3']
+
+      users.forEach(el => {
+        // d3.select("figure#graph").exit().remove();
+
+        d3.select(el)
+          .on("change", () => {
+            this.cleanSVG();
+            this.drawGraph(this.randomData());
+          })
+      })
 
     })
 
   }
 
 
-  cleanSVG(){
-    d3.selectAll('circle').remove() ; 
-    d3.selectAll('line').remove() ; 
+  cleanSVG() {
+    d3.selectAll('circle').remove();
+    d3.selectAll('line').remove();
   }
 
-  getUnique(graphNodes: any){
+  getUnique(graphNodes: any) {
     var lookup: any;
     var items = graphNodes;
     var result = [];
     console.log(graphNodes);
-    
+
     for (var item, i = 0; item = items[i++];) {
       var name = item.group;
 
@@ -113,72 +124,102 @@ export class GraphChartComponent {
         lookup[name] = 1;
         result.push(name);
       }
-}
+    }
   }
 
-  getRandomColor(numberColors : any) {
+  getRandomColor(numberColors: any) {
     var a = []
     var letters = '0123456789ABCDEF';
-    
-    for(var j=0 ; j< numberColors ; j++){
+
+    for (var j = 0; j < numberColors; j++) {
       var color = '#';
-        for (var i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
-        }
-    a.push(color)
-  }
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      a.push(color)
+    }
     return a;
   }
 
-  randomData (){
-    var data : any ; 
+  randomData() {
+    var data: any;
     this.graphChart.get_graph().then((res: any) => {
-      this.graph = res ; 
+      this.graph = res;
     })
     return this.graph
-    
     // return data 
-  } 
+  }
 
-   drawGraph(graph : any){
-            this.simulation = d3.forceSimulation()
-            .force("link", d3.forceLink().id(function(d: any) { return d.id; }))
-            .force("charge", d3.forceManyBody())
-            .force("center", d3.forceCenter(this.width / 2, this.height / 2));
-      // console.log(graph);
-      
-        var link = this.svg.append("g")
-        // .attr("class", "links")
-        .attr("stroke", "#000")
-        // .attr("stroke-opacity", 0.6)
-        .selectAll("line")
-        .data(graph.links)
-        .enter().append("line")
-        .attr("stroke-width", function(d: any) { return (d.value/10); })
-        // .attr("stroke-width", function(d: any) { return Math.sqrt(d.value); });
+  drawGraph(graph: any) {
+
+    const tooltip = d3.select("#graph")
+      .append("div")
+      .attr("class", "d3-tooltip")
+      .style("position", "absolute")
+      .style("z-index", "100")
+      .style("visibility", "hidden")
+      .style("padding", "15px")
+      .style("background", "rgba(0,0,0,0.6)")
+      .style("border-radius", "5px")
+      .style("color", "#fff")
+      .text("a simple tooltip");
+
+    this.simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().id(function (d: any) {
+        return d.id;
+      }))
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(this.width / 2, this.height / 2));
+    // console.log(graph);
+
+    var link = this.svg.append("g")
+      // .attr("class", "links")
+      .attr("stroke", "#000")
+      // .attr("stroke-opacity", 0.6)
+      .selectAll("line")
+      .data(graph.links)
+      .enter().append("line")
+      .attr("stroke-width", function (d: any) {
+        return (d.value / 10);
+      })
+      .on('mouseover', (event: any, d: any) => {
+        console.log(d);
         
-        
-        var node =  this.svg.append("g")
-        // .attr("class", "nodes")
-        .attr("stroke", "#fff")
-        .attr("stroke-opacity", "1.5px")
-        .selectAll("circle")
-        .data(graph.nodes)
-        .enter().append("circle")
-        .attr("r", (d: any) => { return Math.log(d.Count) + 2  ; })
-        .attr("fill", (d: any) => { return this.color(d.group); })
-        .call(d3.drag()
+        tooltip.html(`Interaction: ${d.value}`).style("visibility", "visible");
+        d3.select(event.currentTarget).transition()
+          .duration(50)
+          .attr('opacity', '.75')
+          // d3.select("circle").transition()
+          //   .duration(750)
+          // .attr("r", 16);
+      })
+    // .attr("stroke-width", function(d: any) { return Math.sqrt(d.value); });
+
+    var node = this.svg.append("g")
+      // .attr("class", "nodes")
+      .attr("stroke", "#fff")
+      .attr("stroke-opacity", "1.5px")
+      .selectAll("circle")
+      .data(graph.nodes)
+      .enter().append("circle")
+      .attr("r", (d: any) => {
+        return Math.log(d.Count) + 2;
+      })
+      .attr("fill", (d: any) => {
+        return this.color(d.group);
+      })
+      .call(d3.drag()
         .on("start", (d: any) => {
-                  this.simulation.alphaTarget(0.3).restart();
-                  d.fx = d.x;
-                  d.fy = d.y;
-                  // this.draggedNode = d;
+          this.simulation.alphaTarget(0.3).restart();
+          d.fx = d.x;
+          d.fy = d.y;
+          // this.draggedNode = d;
         })
         .on("drag", (event, d: any) => {
-          d.fx = d3.pointer(event)[0] - 3*(this.width/5)
-          d.fy = d3.pointer(event)[1] - 6*(this.height/7)
+          d.fx = d3.pointer(event)[0] - 3 * (this.width / 5)
+          d.fy = d3.pointer(event)[1] - 6 * (this.height / 7)
           this.simulation.alphaTarget(0.3).restart();
-          
+
         })
         .on("end", (d) => {
           this.simulation.alphaTarget(0);
@@ -186,46 +227,68 @@ export class GraphChartComponent {
           d.fy = null;
           // this.draggedNode = null;
         }))
-        // .on('mouseover', (d: any) => {
-        //       console.log(node);
-              
-        //       d3.select("circle").transition()
-        //       .duration(750)
-        //       .attr("r", 16);
-        // })
-        // .on('mouseout', () => {
-        //   d3.select("circle").transition()
+      .on('mouseover', (event: any, d: any) => {
+        tooltip.html(`Website: ${d.id}`).style("visibility", "visible");
+        d3.select(event.currentTarget).transition()
+          .duration(50)
+          .attr('opacity', '.75')
+          // d3.select("circle").transition()
+          //   .duration(750)
+          .attr("r", 16);
+      })
+      .on("mousemove", (event: any, d: any) => {
+        tooltip
+          .style("top", (event.pageY - 10) + "px")
+          .style("left", (event.pageX + 10) + "px");
+      })
+      .on('mouseout', (event: any, d: any) => {
+        tooltip.style('visibility', 'hidden')
+        d3.select(event.currentTarget)
+          .attr("r", Math.log(d.Count) + 2)
+          .attr('opacity', '1');
+        // d3.select("circle").transition()
         //   .duration(750)
         //   .attr("r", 8);
-        // })
-        
-        
+      })
 
-        node.append("title")
-        .text(function(d: any) { return d.id; });
+    // node.append("title")
+    //   .text(function (d: any) {
+    //     return d.id;
+    //   });
+
+    this.simulation
+      .nodes(graph.nodes)
+      .on("tick", () => {
+        link
+          .attr("x1", function (d: any) {
+            return d.source.x;
+          })
+          .attr("y1", function (d: any) {
+            return d.source.y;
+          })
+          .attr("x2", function (d: any) {
+            return d.target.x;
+          })
+          .attr("y2", function (d: any) {
+            return d.target.y;
+          });
+
+        node
+          .attr("cx", function (d: any) {
+            return d.x;
+          })
+          .attr("cy", function (d: any) {
+            return d.y;
+          });
+      });
+
+    this.simulation.force("link")
+      .links(graph.links);
+
+    // d3.selectAll("line")
 
 
-        this.simulation
-        .nodes(graph.nodes)
-        .on("tick", () => {
-          link
-              .attr("x1", function(d: any) { return d.source.x; })
-              .attr("y1", function(d: any) { return d.source.y; })
-              .attr("x2", function(d: any) { return d.target.x; })
-              .attr("y2", function(d: any) { return d.target.y; });
-      
-          node
-              .attr("cx", function(d: any) { return d.x; })
-              .attr("cy", function(d: any) { return d.y; });
-        });
-
-        this.simulation.force("link")
-        .links(graph.links);
-        
-        // d3.selectAll("line")
-
-
-   }
+  }
 
 
 }
